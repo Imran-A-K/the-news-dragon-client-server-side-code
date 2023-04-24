ef6865e9156ad90000cd36f2700447388c53f7bd
Imran-A-K <imran.k.professional@gmail.com>
2023-04-24 12:14:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json')
+
+app.use(cors());
+
+app.get('/', (req,res) =>{
+    res.send('News Dragon is Running')
+});
+
+app.get('/categories', (req,res) =>{
+    res.send(categories)
+})
+
+app.listen(port, () =>{
+    console.log(`Dragon API is running on port: ${port}`)
+})
