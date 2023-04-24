21ab31b828e7b24ecb6ae81922ccec8588d24e99
Imran-A-K <imran.k.professional@gmail.com>
2023-04-24 23:43:50 +0600

    completed local api of this server

--- index.js
+++ index.js
@@ -18,6 +18,35 @@ app.get('/categories', (req,res) =>{
 app.get('/news', (req,res) =>{
     res.send(news)
 })
+app.get('/news/:id' , (req,res) =>{
+    const id = req.params.id;
+    // console.log(id)
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+})
+// app.get('/categories/:id', (req,res) => {
+//     const id = req.params.id;
+//     // console.log(id)
+//     if(id == 0){
+//         res.send(news);
+//     }
+//     else{
+//         const selectedNews = news.find( n => n._id === id);
+//     res.send(selectedNews);
+//     }
+// })
+
+app.get('/categories/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id)
+    if(id == 0){
+        res.send(news);
+    }
+    else{
+        const selectedNews = news.find( n => parseInt(n._id) === id);
+    res.send(selectedNews);
+    }
+})
 
 app.listen(port, () =>{
     console.log(`Dragon API is running on port: ${port}`)
